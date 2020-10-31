3477f3c467ba5ba1dea8345199005cdafc61fab2
analauramoyano <40239185+analauramoyano@users.noreply.github.com>
2020-10-31 03:19:10 -0300

    formato

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -18,6 +18,7 @@ export class ProductComponent implements OnInit, DoCheck, OnDestroy{
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
+    today = new Date();
 
     constructor()
     {
